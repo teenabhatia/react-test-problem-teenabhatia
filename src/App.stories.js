bf2b3797589c4de67beaa45ed8671ce04d358202
teenabhatia <63713718+teenabhatia@users.noreply.github.com>
2023-03-13 18:18:18 -0400

    update app and stories

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,2 +1,14 @@
 // Stories for testing `App` go here
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
+
+import React from 'react';
+
+import App from './App';
+
+export default {
+  title: 'App',
+  component: App,
+} 
+
+const Template = (args) => <App {...args} />;
+export const Default = Template.bind({});
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,37 @@
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 
 // import your story from App.stories.js
+import { Default } from './App.stories';
 
 
 // Add your tests here
+
+it('renders the default button label', () => {
+    const { getByText } = render(<Default />);
+    expect(getByText('Click me')).toBeInTheDocument();
+  });
+  
+  it('changes the button label on click', () => {
+    const { getByText } = render(<Default />);
+    const button = getByText('Click me');
+    fireEvent.click(button);
+    expect(button).toHaveTextContent('Thanks!');
+  });
+
+  it('changes the button label back on click', () => {
+    const { getByText } = render(<Default />);
+    const button = getByText('Click me');
+    
+    // click once to change text to "Thanks!"
+    fireEvent.click(button);
+    expect(button).toHaveTextContent('Thanks!');
+  
+    // click again to change text back to "Click me"
+    fireEvent.click(button);
+    expect(button).toHaveTextContent('Click me');
+  });
+  
+
+
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
